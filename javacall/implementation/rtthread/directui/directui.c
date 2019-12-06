748f78beb21a4a62d51074afd471a2e079b27551
M@x <ruitao.mu@gmail.com>
2019-12-07 00:24:08 +0800

    Directui implementation

--- javacall/implementation/rtthread/directui/directui.c
+++ javacall/implementation/rtthread/directui/directui.c
@@ -22,6 +22,7 @@
 #include "javacall_directui.h"
 #include <rtthread.h>
 #include <fcntl.h>
+#include "DefineCharLib.h"
 
 #ifdef __cplusplus
 extern "C"{
@@ -39,36 +40,9 @@ static int fd = -1;
 #define LCD_CTRL_GET_STATUS				3		 //Ê∂≤Êô∂Áä∂ÊÄÅËØªÂèñ
 #define LCD_CTRL_OPEN_DEV					4    //Ê∂≤Êô∂Âî§ÈÜíÊâìÂºÄÔºàarg=ZX_NULLÔºâ
 #define LCD_CTRL_CLOSE_DEV				5		 //Ê∂≤Êô∂ÊéâÁîµÂÖ≥Èó≠Ôºàarg=ZX_NULLÔºâ
-
-const static rt_uint8_t BigGB_B5B1[48]=
-{0x00,0x00,0x04,0x02,0x01,0x00,0x00,0x0F,0x00,0x00,0x00,0x01,0x06,0x00,0x00,0x00,
-0x00,0x20,0x22,0x22,0xA2,0x22,0x22,0xE2,0x22,0x22,0xA2,0x22,0x3F,0x00,0x00,0x00,
-0x00,0x20,0x20,0x20,0x20,0x20,0x20,0x20,0x20,0x20,0x20,0x20,0xF0,0x00,0x00,0x00}; /*ÂΩì*/
-
-const static rt_uint8_t BigGB_C7B0[48]=
-{0x01,0x01,0x01,0x09,0x07,0x01,0x01,0x01,0x01,0x01,0x03,0x0D,0x01,0x01,0x01,0x00,
-0x00,0x00,0x7F,0x49,0x49,0x49,0x7F,0x00,0x00,0x3F,0x00,0x00,0x7F,0x00,0x00,0x00,
-0x00,0x00,0xF0,0x00,0x20,0x10,0xE0,0x00,0x00,0x00,0x20,0x10,0xE0,0x00,0x00,0x00};	/*Ââç*/
-
-const static rt_uint8_t BigGB_D5FD[48]=
-{0x00,0x04,0x04,0x04,0x04,0x04,0x04,0x07,0x04,0x04,0x04,0x04,0x04,0x04,0x00,0x00,
-0x00,0x00,0x00,0x3F,0x00,0x00,0x00,0xFF,0x10,0x10,0x10,0x10,0x10,0x00,0x00,0x00,
-0x20,0x20,0x20,0xE0,0x20,0x20,0x20,0xE0,0x20,0x20,0x20,0x20,0x20,0x20,0x20,0x00};	/*Ê≠£*/
-
-const static rt_uint8_t BigGB_CFF2[48]=
-{0x00,0x01,0x01,0x01,0x03,0x05,0x09,0x01,0x01,0x01,0x01,0x01,0x01,0x01,0x00,0x00,
-0x00,0xFF,0x00,0x00,0x00,0x3F,0x21,0x21,0x21,0x3F,0x00,0x00,0x00,0xFF,0x00,0x00,
-0x00,0xF0,0x00,0x00,0x00,0x80,0x00,0x00,0x00,0x80,0x00,0x20,0x10,0xE0,0x00,0x00};	/*Âêë*/
-
-const static rt_uint8_t BigGB_D3D0[48]=
-{0x02,0x02,0x02,0x02,0x02,0x03,0x0E,0x02,0x02,0x02,0x02,0x02,0x02,0x02,0x02,0x00,
-0x02,0x04,0x08,0x10,0x7F,0xC9,0x49,0x49,0x49,0x49,0x49,0x7F,0x00,0x00,0x00,0x00,
-0x00,0x00,0x00,0x00,0xF0,0x00,0x00,0x00,0x00,0x20,0x10,0xE0,0x00,0x00,0x00,0x00}; /*Êúâ*/
-
-const static rt_uint8_t BigGB_B9A6[48]=
-{0x01,0x01,0x01,0x01,0x01,0x01,0x01,0x00,0x00,0x0F,0x00,0x00,0x00,0x00,0x00,0x00,
-0x00,0x00,0x00,0xFF,0x01,0x01,0x01,0x80,0x87,0xF8,0x80,0x80,0x80,0xFF,0x00,0x00,
-0x80,0xC0,0x80,0x80,0x00,0x10,0x20,0xC0,0x00,0x00,0x20,0x10,0x20,0xC0,0x00,0x00};	/*Âäü*/
+#define LCD_CTRL_UPDATE_SECTION			6
+#define LCD_CTRL_FLUSH					7
+#define LCD_CTRL_CLEAR_SECTION			8
 
 struct rt_lcd_zone_info
 {
@@ -109,9 +83,9 @@ javacall_result javacall_directui_finalize(void) {
 }
 
 javacall_result javacall_directui_get_screen(int* screen_width, int* screen_height) {
-    *screen_width = 208;
+    *screen_width = DISPLAY_BUFFER_COLUMU;
     *screen_height = 80;
-    return JAVACALL_FAIL;
+    return JAVACALL_OK;
 }
 
 void javacall_directui_clear(int rgb) {
@@ -129,6 +103,7 @@ void javacall_directui_clear(int rgb) {
 }
 
 javacall_result javacall_directui_flush() {
+	ioctl(fd, LCD_CTRL_FLUSH, NULL);
     return JAVACALL_OK;
 }
 
@@ -136,52 +111,420 @@ javacall_result javacall_directui_flush_region(int xstart, int ystart, int xend,
 	return JAVACALL_OK;
 }
 
+static int valid_font(int size) {
+	if (size < 2) {
+		return 2;
+	} else if (size > 4){
+		return 4;
+	}
+	return size;
+}
+
+typedef struct {
+	javacall_utf16 unicode;
+	rt_uint8_t* data;
+} FontInfo;
+
+#define UNI_GB_B6C1    (u_int)0x8bfb
+#define UNI_GB_BFA8    (u_int)0x5361
+#define UNI_GB_D6D0    (u_int)0x4e2d
+#define UNI_GB_B3C9    (u_int)0x6210
+#define UNI_GB_B9A6    (u_int)0x529f
+#define UNI_GB_CAA7    (u_int)0x5931
+#define UNI_GB_B0DC    (u_int)0x8d25
+#define UNI_GB_C0AD    (u_int)0x62c9
+#define UNI_GB_D5A2    (u_int)0x95f8
+#define UNI_GB_C7EB    (u_int)0x8bf7
+#define UNI_GB_B9BA    (u_int)0x8d2d
+#define UNI_GB_B5E7    (u_int)0x7535
+#define UNI_GB_CDB8    (u_int)0x900f
+#define UNI_GB_D6A7    (u_int)0x652f
+#define UNI_GB_B6DA    (u_int)0x56e4
+#define UNI_GB_BBFD    (u_int)0x79ef
+#define UNI_GB_C4E6    (u_int)0x9006
+#define UNI_GB_CFE0    (u_int)0x76f8
+#define UNI_GB_D0F2    (u_int)0x5e8f
+#define UNI_GB_C9CF    (u_int)0x4e0a
+#define UNI_GB_D2BB    (u_int)0x4e00
+#define UNI_GB_B6FE    (u_int)0x4e8c
+#define UNI_GB_C8FD    (u_int)0x4e09
+#define UNI_GB_CBC4    (u_int)0x56db
+#define UNI_GB_CEE5    (u_int)0x4e94
+#define UNI_GB_C1F9    (u_int)0x516d
+#define UNI_GB_C6DF    (u_int)0x4e03
+#define UNI_GB_B0CB    (u_int)0x516b
+#define UNI_GB_BEC5    (u_int)0x4e5d
+#define UNI_GB_CAAE    (u_int)0x5341
+#define UNI_GB_D4C2    (u_int)0x6708
+#define UNI_GB_D5FD    (u_int)0x6b63
+#define UNI_GB_B7B4    (u_int)0x53cd
+#define UNI_GB_CFF2    (u_int)0x5411
+#define UNI_GB_D3D0    (u_int)0x6709
+#define UNI_GB_CEDE    (u_int)0x65e0
+#define UNI_GB_C1BF    (u_int)0x91cf
+#define UNI_GB_D1B9    (u_int)0x538b
+#define UNI_GB_C1F7    (u_int)0x6d41
+#define UNI_GB_D7DC    (u_int)0x603b
+#define UNI_GB_B5B1    (u_int)0x5f53
+#define UNI_GB_C7B0    (u_int)0x524d
+#define UNI_GB_C8D5    (u_int)0x65e5
+#define UNI_GB_C6DA    (u_int)0x671f
+#define UNI_GB_CAB1    (u_int)0x65f6
+#define UNI_GB_BCE4    (u_int)0x95f4
+#define UNI_GB_CAA3    (u_int)0x5269
+#define UNI_GB_D3E0    (u_int)0x4f59
+#define UNI_GB_BDF0    (u_int)0x91d1
+#define UNI_GB_B6EE    (u_int)0x989d
+#define UNI_GB_B1ED    (u_int)0x8868
+#define UNI_GB_BAC5    (u_int)0x53f7
+#define UNI_GB_B5D8    (u_int)0x5730
+#define UNI_GB_D6B7    (u_int)0x5740
+#define UNI_GB_C2CA    (u_int)0x7387
+#define UNI_GB_D2F2    (u_int)0x56E0
+#define UNI_GB_CAFD    (u_int)0x6570
+#define UNI_GB_C4DC    (u_int)0x80fd
+
+#define BIG_CHINESE_FONT_ENTRY(x) {UNI_GB_##x, BigGB_##x}
+
+static FontInfo BigChineseFontTable[] = {
+	/*…œ*/BIG_CHINESE_FONT_ENTRY(C9CF),
+	/*“ª*/BIG_CHINESE_FONT_ENTRY(D2BB),
+	/*∂˛*/BIG_CHINESE_FONT_ENTRY(B6FE),
+	/*»˝*/BIG_CHINESE_FONT_ENTRY(C8FD),
+	/*Àƒ*/BIG_CHINESE_FONT_ENTRY(CBC4),
+	/*ŒÂ*/BIG_CHINESE_FONT_ENTRY(CEE5),
+	/*¡˘*/BIG_CHINESE_FONT_ENTRY(C1F9),
+	/*∆ﬂ*/BIG_CHINESE_FONT_ENTRY(C6DF),
+	/*∞À*/BIG_CHINESE_FONT_ENTRY(B0CB),
+	/*æ≈*/BIG_CHINESE_FONT_ENTRY(BEC5),
+	/* Æ*/BIG_CHINESE_FONT_ENTRY(CAAE),
+	/*‘¬*/BIG_CHINESE_FONT_ENTRY(D4C2),
+	/*’˝*/BIG_CHINESE_FONT_ENTRY(D5FD),
+	/*∑¥*/BIG_CHINESE_FONT_ENTRY(B7B4),
+	/*œÚ*/BIG_CHINESE_FONT_ENTRY(CFF2),
+	/*”–*/BIG_CHINESE_FONT_ENTRY(D3D0),
+	/*Œﬁ*/BIG_CHINESE_FONT_ENTRY(CEDE),	
+	/*π¶*/BIG_CHINESE_FONT_ENTRY(B9A6),
+	/*µÁ*/BIG_CHINESE_FONT_ENTRY(B5E7),	
+	/*¡ø*/BIG_CHINESE_FONT_ENTRY(C1BF),
+	/*—π*/BIG_CHINESE_FONT_ENTRY(D1B9),
+	/*¡˜*/BIG_CHINESE_FONT_ENTRY(C1F7),
+	/*◊‹*/BIG_CHINESE_FONT_ENTRY(D7DC),
+	/*µ±*/BIG_CHINESE_FONT_ENTRY(B5B1),
+	/*«∞*/BIG_CHINESE_FONT_ENTRY(C7B0),
+	/*»’*/BIG_CHINESE_FONT_ENTRY(C8D5),
+	/*∆⁄*/BIG_CHINESE_FONT_ENTRY(C6DA),
+	/* ±*/BIG_CHINESE_FONT_ENTRY(CAB1),
+	/*º‰*/BIG_CHINESE_FONT_ENTRY(BCE4),	
+	/*Õ∏*/BIG_CHINESE_FONT_ENTRY(CDB8),
+	/*÷ß*/BIG_CHINESE_FONT_ENTRY(D6A7),
+	/* £*/BIG_CHINESE_FONT_ENTRY(CAA3),
+	/*”‡*/BIG_CHINESE_FONT_ENTRY(D3E0),
+	/*Ω*/BIG_CHINESE_FONT_ENTRY(BDF0),
+	/*∂Ó*/BIG_CHINESE_FONT_ENTRY(B6EE),
+	/*±Ì*/BIG_CHINESE_FONT_ENTRY(B1ED),
+	/*∫≈*/BIG_CHINESE_FONT_ENTRY(BAC5),
+	/*µÿ*/BIG_CHINESE_FONT_ENTRY(B5D8),
+	/*÷∑*/BIG_CHINESE_FONT_ENTRY(D6B7),	
+	/*œ‡*/BIG_CHINESE_FONT_ENTRY(CFE0),
+	/*¬ */BIG_CHINESE_FONT_ENTRY(C2CA),
+	/*“Ú*/BIG_CHINESE_FONT_ENTRY(D2F2),
+	/* ˝*/BIG_CHINESE_FONT_ENTRY(CAFD),
+	/*ƒ‹*/BIG_CHINESE_FONT_ENTRY(C4DC)
+};
+
+#define SMALL_CHINESE_FONT_ENTRY(x) {UNI_GB_##x, SmallGB_##x}
+
+static FontInfo SmallChineseFontTable[] = {
+	
+	/*∂¡*/SMALL_CHINESE_FONT_ENTRY(B6C1),
+	/*ø®*/SMALL_CHINESE_FONT_ENTRY(BFA8),
+	/*÷–*/SMALL_CHINESE_FONT_ENTRY(D6D0),
+	/*≥…*/SMALL_CHINESE_FONT_ENTRY(B3C9),
+	/*π¶*/SMALL_CHINESE_FONT_ENTRY(B9A6),
+	/* ß*/SMALL_CHINESE_FONT_ENTRY(CAA7),
+	/*∞‹*/SMALL_CHINESE_FONT_ENTRY(B0DC),
+	/*¿≠*/SMALL_CHINESE_FONT_ENTRY(C0AD),
+	/*’¢*/SMALL_CHINESE_FONT_ENTRY(D5A2),
+	/*«Î*/SMALL_CHINESE_FONT_ENTRY(C7EB),
+	/*π∫*/SMALL_CHINESE_FONT_ENTRY(B9BA),
+	/*µÁ*/SMALL_CHINESE_FONT_ENTRY(B5E7),
+	/*Õ∏*/SMALL_CHINESE_FONT_ENTRY(CDB8),
+	/*÷ß*/SMALL_CHINESE_FONT_ENTRY(D6A7),
+	/*∂⁄*/SMALL_CHINESE_FONT_ENTRY(B6DA),
+	/*ª˝*/SMALL_CHINESE_FONT_ENTRY(BBFD),
+	/*ƒÊ*/SMALL_CHINESE_FONT_ENTRY(C4E6),
+	/*œ‡*/SMALL_CHINESE_FONT_ENTRY(CFE0),
+	/*–Ú*/SMALL_CHINESE_FONT_ENTRY(D0F2)
+	
+};
+
+static FontInfo UnitFontsTable[] = {
+	{UYuan, UYuanPicture},
+	{UWYuan, UWYuanPicture},
+	{UdegreeA, UdegreeAPicture},
+	{UdegreeC, UdegreeCPicture},
+	{UW, UWPicture},
+	{UkW, UkWPicture},
+	{UVA, UVAPicture},
+	{UkVA, UkVAPicture},
+	{Uvar, UvarPicture},
+	{Ukvar, UkvarPicture},
+	{UkWh, UkWhPicture},
+	{UkVAh, UkVAhPicture},
+	{Ukvarh, UkvarhPicture},
+	{UA, UAPicture},
+	{UV, UVPicture},
+	{UHz, UHzPicture},
+	{U1PWh, U1PWhPicture},
+	{U1Pvarh, U1PvarhPicture},
+	{U1PVAh, U1PVAhPicture},
+	{UAh, UAhPicture}
+};
+
+static rt_uint8_t* PictureFonts[] = {
+	LabFlagPicture,
+	SetupKeyPicture,
+	MiMaErrorPicture,
+	DianChi1Picture,
+	DianChi2Picture,
+	FeilvDianJiaTPicture,
+	JieTiDianJiaLPicture,
+	UaPicture,
+	UbPicture,
+	UcPicture,
+	_IaPicture,
+	IaPicture,
+	_IbPicture,
+	IbPicture,
+	_IcPicture,
+	IcPicture,
+	FaSongPicture,
+	FaSong1Picture,
+	FaSong2Picture,
+	SignalGrade0Picture,
+	SignalGrade1Picture,
+	SignalGrade2Picture,
+	SignalGrade3Picture,
+	SignalGrade4Picture,
+	ZaiBoFaSongPicture,
+	ZaiBoFaSongUpPicture,
+	ZaiBoFaSongDownPicture,
+	ZaiBoFaSongBothPicture
+};
+
+static rt_uint8_t* SmallNumberFonts[] = {
+	SmallNumber0,
+	SmallNumber1,
+	SmallNumber2,
+	SmallNumber3,
+	SmallNumber4,
+	SmallNumber5,
+	SmallNumber6,
+	SmallNumber7,
+	SmallNumber8,
+	SmallNumber9,
+	SmallNumberA,
+	SmallNumberB,
+	SmallNumberC,
+	SmallNumberD,
+	SmallNumberE,
+	SmallNumberF,
+};
+
+static rt_uint8_t* BigNumberFonts[] = {
+	BigNumber0,
+	BigNumber1,
+	BigNumber2,
+	BigNumber3,
+	BigNumber4,
+	BigNumber5,
+	BigNumber6,
+	BigNumber7,
+	BigNumber8,
+	BigNumber9,
+	BigNumberA,
+	BigNumberB,
+	BigNumberC,
+	BigNumberD,
+	BigNumberE,
+	BigNumberF
+};
+
+static rt_uint8_t* get_Chinese_fonts(int font, javacall_utf16 unicode) {
+	int table_len, i;
+	FontInfo* table;
+	
+	if (font == 2) {
+		table_len = sizeof(SmallChineseFontTable)/sizeof(FontInfo);
+		table = SmallChineseFontTable;
+	} else if (font == 3) {
+		table_len = sizeof(BigChineseFontTable)/sizeof(FontInfo);
+		table = BigChineseFontTable;
+	} else {
+		return NULL;
+	}
+	
+	for (i = 0; i < table_len; i++) {
+		if (table[i].unicode == unicode) {
+			return table[i].data;
+		}
+	}
+	
+	return NULL;
+}
+
+static rt_uint8_t* get_font_data(int font, javacall_utf16 unicode) {
+	if ((unicode >= LabFlag) && (unicode <= ZaiBoFaSongBoth)) {
+		if (font == 2) {
+			rt_kprintf("get_font_data: %d\n", unicode);
+			return PictureFonts[unicode - LabFlag];
+		} else {
+			return NULL;
+		}
+	}
+	
+	if ((unicode >= 0x0200) && (unicode <= 0x02FF)) {
+		if (font == 2) {
+			int i;
+			FontInfo* table = UnitFontsTable;
+			int table_len = sizeof(UnitFontsTable)/sizeof(FontInfo);
+			for (i = 0; i < table_len; i++) {
+				if (table[i].unicode == unicode) {
+					return table[i].data;
+				}
+			}
+		}
+		return NULL;
+	}
+	
+	if ((unicode >= (javacall_utf16)'0') && ((unicode <= (javacall_utf16)'9'))) {
+		if (font == 2) {
+			return SmallNumberFonts[unicode - (javacall_utf16)'0'];
+		} else if (font == 4) {
+			return BigNumberFonts[unicode - (javacall_utf16)'0'];
+		} else {
+			return NULL;
+		}
+	}
+	
+	if ((unicode >= (javacall_utf16)'A') && ((unicode <= (javacall_utf16)'F'))) {
+		if (font == 2) {
+			return SmallNumberFonts[unicode - (javacall_utf16)'A' + 10];
+		} else if (font == 4) {
+			return BigNumberFonts[unicode - (javacall_utf16)'A' + 10];
+		} else {
+			return NULL;
+		}
+	}
+
+	if (unicode == (javacall_utf16)'.') {
+		if (font == 4) {
+			return BigNumberSPoint;
+		} else {
+			return NULL;
+		}
+	}
+
+	if (unicode == (javacall_utf16)':') {
+		if (font == 4) {
+			return BigNumberTPoint;
+		} else {
+			return NULL;
+		}
+	}
+
+	if (unicode == (javacall_utf16)'-') {
+		if (font == 4) {
+			return BigNumberNegative;
+		} else {
+			return NULL;
+		}
+	}
+	
+	return get_Chinese_fonts(font, unicode);
+}
+
+static get_margin(int font) {
+	return font == 4?2:0;
+}
+
 javacall_result javacall_directui_textout(int font, int color, int x, int y,
         const javacall_utf16* text, int textLen, int delayed) {
 	int i;
 	struct rt_lcd_refresh_param refresh_param;
 	
+	font = valid_font(font);
+	
 	refresh_param.zone_info.col_start = x;
 	refresh_param.zone_info.row_start = y;
-	refresh_param.zone_info.row_end = refresh_param.zone_info.row_start + 3;
-	for (i = 0; i < textLen; i++) {
-		int skip = 0;
-		refresh_param.zone_info.col_end = refresh_param.zone_info.col_start + 16;
+	refresh_param.zone_info.row_end = refresh_param.zone_info.row_start + font;
+	for (i = 0; (i < textLen) && (refresh_param.zone_info.col_start < DISPLAY_BUFFER_COLUMU); i++) {
+		int w, h;
+		javacall_directui_text_getsize(font, text+i, 1, &w, &h);
+		refresh_param.zone_info.col_end = refresh_param.zone_info.col_start + w - get_margin(font);
+		rt_kprintf("javacall_directui_textout: 0x%x, font=%d, width=%d\n", text[i], font, w);
 		
-		switch (text[i]) {
-		case 0x5f53:
-			refresh_param.p_data = (rt_uint8_t *)BigGB_B5B1;
-			break;
-		case 0x524d:
-			refresh_param.p_data = (rt_uint8_t *)BigGB_C7B0;
-			break;
-		case 0x6b63:
-			refresh_param.p_data = (rt_uint8_t *)BigGB_D5FD;
-			break;
-		case 0x5411:
-			refresh_param.p_data = (rt_uint8_t *)BigGB_CFF2;
-			break;
-		case 0x6709:
-			refresh_param.p_data = (rt_uint8_t *)BigGB_D3D0;
-			break;
-		case 0x529f:
-			refresh_param.p_data = (rt_uint8_t *)BigGB_B9A6;
-			break;
-		default:
-			skip = 1;
+		refresh_param.p_data = get_font_data(font, text[i]);
+		if (refresh_param.p_data != NULL) {
+			ioctl(fd, LCD_CTRL_UPDATE_SECTION, &refresh_param);
+		} else {
+			struct rt_lcd_set_param set_param;
+
+			memcpy(&set_param.zone_info, &refresh_param.zone_info, sizeof(struct rt_lcd_zone_info));
+			set_param.value = 0x0;
+
+			ioctl(fd, LCD_CTRL_CLEAR_SECTION, &set_param);
 		}
-		if (!skip) {
-			ioctl(fd, LCD_CTRL_REFRESH_SECTION, &refresh_param);
-		}
-		refresh_param.zone_info.col_start = refresh_param.zone_info.col_end;
+		refresh_param.zone_info.col_start = refresh_param.zone_info.col_end + get_margin(font);
+	}
+
+	if (!delayed) {
+		ioctl(fd, LCD_CTRL_FLUSH, NULL);
 	}
 	
 	return JAVACALL_OK;
 }
 
+static int get_char_width(int font, javacall_utf16 unicode) {
+	int size;
+	
+	if ((unicode >= 0x0100) && (unicode <= 0x01FF)) {
+		//PictureChar
+		size = 12;
+	} else if ((unicode >= 0x0200) && (unicode <= 0x02FF)) {
+		//UnitChar
+		size = 20;
+	} else if (font == 2) {
+		size = 12;
+	} else if (font == 3) {
+		size = 16;
+	} else {
+		if ((unicode == (javacall_utf16)'.') || (unicode == (javacall_utf16)':')) {
+			size = 4;
+		} else {
+			size= 14;
+		}
+	}
+
+	return size + get_margin(font);
+}
+
 javacall_result javacall_directui_text_getsize(int font, const javacall_utf16* text,
         int textLen, int* width, int* height) {
-    return JAVACALL_FAIL;
+	
+	int w = 0, i;
+	
+    font = valid_font(font);
+	
+	for (i = 0; i < textLen; i++) {
+		w += get_char_width(font, text[i]);
+	}
+	
+	*width = w;
+	*height = font;
+	
+	return JAVACALL_OK;
 }
 
 javacall_result javacall_directui_image_getsize(javacall_uint8* image_data,
